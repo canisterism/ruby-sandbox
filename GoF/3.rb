7120c09cc1d32c572c53fce289a85f0658d03005
canisterism <ohtthstc@gmail.com>
2019-06-05 18:13:00 +0900

    template method done

--- GoF/3.rb
+++ GoF/3.rb
@@ -1,22 +1,73 @@
+# Template Method
+## 変わらない部分だけ親クラスに定義する
+### 変わる部分は子クラスに
+
 class Report
   def initialize
     @title = '月次報告'
     @text = ['晴天なう','ぽんぽんぺいん']
   end
 
-  def output_report(format = :plane)
-    if format == :plane
-      puts("***#{@title}***")
-      @text.each do |line|
-        puts("***#{line}***")
-      end
-    else
-      puts "<title>#{@title}</title>"
-      puts "<body>"
-      @text.each do |line|
-        puts "<p>#{line}</p>"
-      end
-      puts "</body>"
+  def output_report
+    output_title
+    output_body_start
+    output_body
+    output_body_end
+  end
+
+  # abstract methods
+  def output_title
+    raise 'Called abstract method: output_title'
+  end
+
+  def output_body
+    raise 'Called abstract method: output_body'
+  end
+
+  def output_body_start
+  end
+  def output_body_end
+  end
+
+end
+
+class HTTPReport < Report
+
+  def output_title
+    puts "<title>#{@title}</title>"
+  end
+
+  def output_text
+    @text.each do |line|
+      puts "<p>#{line}</p>"
     end
   end
+
+  def output_body_start
+    puts "<body>"
+  end
+
+
+  def output_body_end
+    puts "</body>"
+  end
+end
+
+class PlainReport < Report
+  def output_title
+    puts "***#{@title}***"
+  end
+
+  def output_text
+    @text.each do |line|
+      puts "***#{@title}***"
+    end
+  end
+end
+
+
+
+
+
+
 end
