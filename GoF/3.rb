a31d4589aedc80d996f991061220d2d87abb914c
canisterism <ohtthstc@gmail.com>
2019-06-05 17:58:48 +0900

    this is shit

--- GoF/3.rb
+++ GoF/3.rb
@@ -1,15 +1,22 @@
 class Report
   def initialize
     @title = '月次報告'
-    @text = ['a','b']
+    @text = ['晴天なう','ぽんぽんぺいん']
   end
 
-  def output_report
-    puts "<title>#{@title}</title>"
-    puts "<body>"
-    @text.each do |line|
-      puts "<p>#{line}</p>"
+  def output_report(format = :plane)
+    if format == :plane
+      puts("***#{@title}***")
+      @text.each do |line|
+        puts("***#{line}***")
+      end
+    else
+      puts "<title>#{@title}</title>"
+      puts "<body>"
+      @text.each do |line|
+        puts "<p>#{line}</p>"
+      end
+      puts "</body>"
     end
-    puts "</body>"
   end
 end
